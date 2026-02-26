49d7f17d894285f518539e3f6206e153ab951d34
Juls Vincent <jvgandrada@gmail.com>
2026-02-26 08:44:12 +0800

    Update ESLint configuration and enhance Input component type definition
    - Disabled the rule '@typescript-eslint/no-empty-object-type' in the ESLint configuration.
    - Added a comment to the InputProps type definition in the Input component to clarify its purpose and compliance with ESLint rules.

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { cn } from '@/lib/utils';
 
+/** Props for the Input component (type alias to satisfy @typescript-eslint/no-empty-object-type) */
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
